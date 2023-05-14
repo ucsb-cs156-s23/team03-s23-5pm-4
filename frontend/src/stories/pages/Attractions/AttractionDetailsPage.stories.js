c4ec2c2a3b42c4e0d2e263fc399be0ccd128adca
SophiaWeiler <sophia.weiler@outlook.com>
2023-05-14 15:54:12 -0700

    sw - added attraction details page + stories + test

---
+++ frontend/src/stories/pages/Attractions/AttractionDetailsPage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AttractionDetailsPage from "main/pages/Attractions/AttractionDetailsPage";
+
+export default {
+    title: 'pages/Attractions/AttractionDetailsPage',
+    component: AttractionDetailsPage
+};
+
+const Template = () => <AttractionDetailsPage />;
+
+export const Default = Template.bind({});
