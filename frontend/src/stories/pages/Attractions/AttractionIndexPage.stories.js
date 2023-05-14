25c14d320d7436d745c8f6556ef015e090308b03
SophiaWeiler <sophia.weiler@outlook.com>
2023-05-14 16:00:53 -0700

    sw - added attraction index page + stories + tests, also added test for attractionUtil

---
+++ frontend/src/stories/pages/Attractions/AttractionIndexPage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AttractionIndexPage from "main/pages/Attractions/AttractionIndexPage";
+
+export default {
+    title: 'pages/Attractions/AttractionIndexPage',
+    component: AttractionIndexPage
+};
+
+const Template = () => <AttractionIndexPage />;
+
+export const Default = Template.bind({});
