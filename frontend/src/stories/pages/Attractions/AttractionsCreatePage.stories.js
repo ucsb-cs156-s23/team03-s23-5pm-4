b88ceb5885938045e4ac57873f08608bb06924e5
SophiaWeiler <sophia.weiler@outlook.com>
2023-05-15 12:06:57 -0700

    sw - adding attractions create page and stories and tests

---
+++ frontend/src/stories/pages/Attractions/AttractionsCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import AttractionCreatePage from "main/pages/Attractions/AttractionCreatePage";
+
+export default {
+    title: 'pages/Attractions/AttractionCreatePage',
+    component: AttractionCreatePage
+};
+
+const Template = () => <AttractionCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
