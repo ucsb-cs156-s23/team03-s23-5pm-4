0d8eba9f873ad6eb04d368598e3616ab3b3ca1f1
SophiaWeiler <sophia.weiler@outlook.com>
2023-05-16 16:13:48 -0700

    sw - adding mock /api/currentUser and /api/systemInfo to attractionIndexPages test

--- frontend/src/tests/pages/Attrractions/AttractionIndexPage.test.js
+++ frontend/src/tests/pages/Attrractions/AttractionIndexPage.test.js
@@ -1,52 +1,72 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import AttractionIndexPage from "main/pages/Attractions/AttractionIndexPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import AttractionIndexPage from "main/pages/Attractions/AttractionIndexPage";
+import mockConsole from "jest-mock-console";
 
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { attractionFixtures } from "fixtures/attractionFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import mockConsole from "jest-mock-console";
 
 
-const mockToast = jest.fn();
-jest.mock('react-toastify', () => {
-    const originalModule = jest.requireActual('react-toastify');
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate
+}));
+
+const mockDelete = jest.fn();
+jest.mock('main/utils/attractionUtils', () => {
     return {
         __esModule: true,
-        ...originalModule,
-        toast: (x) => mockToast(x)
-    };
+        attractionUtils: {
+            del: (id) => {
+                return mockDelete(id);
+            },
+            get: () => {
+                return {
+                    nextId: 5,
+                    attractions: [
+                        {
+                            "id": 3,
+                            "name": "Freebirds",
+                            "address": "879 Embarcadero del Norte",
+                            "city": "Isla Vista",
+                            "state": "CA",
+                            "zip": "93117",
+                            "description": "Burrito joint, and iconic Isla Vista location"
+                        },
+                    ]
+                }
+            }
+        }
+    }
 });
+
 
 describe("AttractionIndexPage tests", () => {
 
+
+    // mock /api/currentUser and /api/systemInfo
     const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
-    const testId = "AttractionTable";
+    const queryClient = new QueryClient();
+    test("renders without crashing", () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AttractionIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
 
-    const setupUserOnly = () => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
-
-    const setupAdminUser = () => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
-
-    test("renders without crashing for regular user", () => {
-        setupUserOnly();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/attractions/all").reply(200, []);
-
+    test("renders correct fields", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -55,74 +75,26 @@ describe("AttractionIndexPage tests", () => {
             </QueryClientProvider>
         );
 
+        const createAttractionButton = screen.getByText("Create Attraction");
+        expect(createAttractionButton).toBeInTheDocument();
+        expect(createAttractionButton).toHaveAttribute("style", "float: right;");
 
+        const name = screen.getByText("Freebirds");
+        expect(name).toBeInTheDocument();
+
+        const description = screen.getByText("Burrito joint, and iconic Isla Vista location");
+        expect(description).toBeInTheDocument();
+
+        expect(screen.getByTestId("AttractionTable-cell-row-0-col-Delete-button")).toBeInTheDocument();
+        expect(screen.getByTestId("AttractionTable-cell-row-0-col-Details-button")).toBeInTheDocument();
+        expect(screen.getByTestId("AttractionTable-cell-row-0-col-Edit-button")).toBeInTheDocument();
     });
 
-    test("renders without crashing for admin user", () => {
-        setupAdminUser();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/attractions/all").reply(200, []);
-
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AttractionIndexPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-
-    });
-
-    test("renders three attractions without crashing for regular user", async () => {
-        setupUserOnly();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/attractions/all").reply(200, attractionFixtures.threeAttractions);
-
-        console.log("OBJECTS: ", attractionFixtures.threeAttractions);
-
-        const { getByTestId } = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AttractionIndexPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
-        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
-        expect(getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
-
-    });
-
-    test("renders three attractions without crashing for admin user", async () => {
-        setupAdminUser();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/attractions/all").reply(200, attractionFixtures.threeAttractions);
-
-        const { getByTestId } = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AttractionIndexPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("2"); });
-        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("3");
-        expect(getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("4");
-
-    });
-
-    test("renders empty table when backend unavailable, user only", async () => {
-        setupUserOnly();
-
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/attractions/all").timeout();
+    test("delete button calls delete and reloads page", async () => {
 
         const restoreConsole = mockConsole();
 
-        const { queryByTestId } = render(
+        render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
                     <AttractionIndexPage />
@@ -130,42 +102,29 @@ describe("AttractionIndexPage tests", () => {
             </QueryClientProvider>
         );
 
-        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+        const name = screen.getByText("Freebirds");
+        expect(name).toBeInTheDocument();
 
-        const errorMessage = console.error.mock.calls[0][0];
-        expect(errorMessage).toMatch("Error communicating with backend via GET on /api/attractions/all");
-        restoreConsole();
+        const description = screen.getByText("Burrito joint, and iconic Isla Vista location");
+        expect(description).toBeInTheDocument();
 
-        expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
-    });
-
-    test("what happens when you click delete, admin", async () => {
-        setupAdminUser();
-
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/attractions/all").reply(200, attractionFixtures.threeAttractions);
-        axiosMock.onDelete("/api/attractions").reply(200, "Attraction with id 1 was deleted");
-
-
-        const { getByTestId } = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AttractionIndexPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
-
-       expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); 
-
-
-        const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+        const deleteButton = screen.getByTestId("AttractionTable-cell-row-0-col-Delete-button");
         expect(deleteButton).toBeInTheDocument();
-       
-        fireEvent.click(deleteButton);
 
-        await waitFor(() => { expect(mockToast).toBeCalledWith("Attraction with id 1 was deleted") });
+        deleteButton.click();
+
+        expect(mockDelete).toHaveBeenCalledTimes(1);
+        expect(mockDelete).toHaveBeenCalledWith(3);
+
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/attractions"));
+
+
+        // assert - check that the console.log was called with the expected message
+        expect(console.log).toHaveBeenCalled();
+        const message = console.log.mock.calls[0][0];
+        const expectedMessage = `AttractionIndexPage deleteCallback: {"id":3,"name":"Freebirds","description":"Burrito joint, and iconic Isla Vista location","address":"879 Embarcadero del Norte"}`;
+        expect(message).toMatch(expectedMessage);
+        restoreConsole();
 
     });
 
