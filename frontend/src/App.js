674cac376ccf7b6e7b84da7c44c7b34b52be9d1b
Anikait Mundhra <ani@169-231-73-67.wireless.ucsb.edu>
2023-05-17 23:49:34 -0700

    finished mergeing over Team01 Restaurant files, all tests pass, and looks good in local host, navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,11 @@ import AttractionDetailsPage from "main/pages/Attractions/AttractionDetailsPage"
 import AttractionEditPage from "main/pages/Attractions/AttractionEditPage";
 import AttractionIndexPage from "main/pages/Attractions/AttractionIndexPage";
 
+import RestaurantCreatePage from "main/pages/Restaurants/RestaurantCreatePage";
+import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
+import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
+import RestaurantDetailsPage from "main/pages/Restaurants/RestaurantDetailsPage";
+
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -52,7 +57,16 @@ function App() {
             </>
           )
         }
-
+        {
+          hasRole(currentUser, "ROLE_USER") && (
+            <>
+              <Route exact path="/restaurants/create" element={<RestaurantCreatePage />} />
+              <Route exact path="/restaurants/edit/:id" element={<RestaurantEditPage />} />
+              <Route exact path="/restaurants/details/:id" element={<RestaurantDetailsPage />} />
+              <Route exact path="/restaurants/" element={<RestaurantIndexPage />} />
+            </>
+          )
+        }
         {
           hasRole(currentUser, "ROLE_USER") && (
             <>
