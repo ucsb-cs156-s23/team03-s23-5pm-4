7203a8547825f69bd7265c8ff970f4ac1a98f74b
SophiaWeiler <sophia.weiler@outlook.com>
2023-05-19 14:20:34 -0700

    sw - adding AttractionCreatePage + test with connected backend

--- frontend/src/tests/pages/Attrractions/AttractionCreatePage.test.js
+++ frontend/src/tests/pages/Attrractions/AttractionCreatePage.test.js
@@ -1,29 +1,46 @@
-import { render, screen, fireEvent, act, waitFor } from "@testing-library/react";
+import { render, waitFor, fireEvent } from "@testing-library/react";
 import AttractionCreatePage from "main/pages/Attractions/AttractionCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import mockConsole from "jest-mock-console";
 
-const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockNavigate
-}));
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 
-const mockAdd = jest.fn();
-jest.mock('main/utils/attractionUtils', () => {
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
     return {
         __esModule: true,
-        attractionUtils: {
-            add: () => { return mockAdd(); }
-        }
-    }
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
 });
 
 describe("AttractionCreatePage tests", () => {
 
-    const queryClient = new QueryClient();
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    beforeEach(() => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    });
+
     test("renders without crashing", () => {
+        const queryClient = new QueryClient();
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -33,60 +50,59 @@ describe("AttractionCreatePage tests", () => {
         );
     });
 
-    test("redirects to /attractions on submit", async () => {
+    test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
 
-        const restoreConsole = mockConsole();
+        const queryClient = new QueryClient();
+        const attraction = {
+            id: 17,
+            name: "Inkstriker",
+            description: "kart",
+            address: "1000"
+        };
 
-        mockAdd.mockReturnValue({
-            "attraction": {
-                id: 3,
-                name: "South Coast Deli",
-                description: "Sandwiches and Salads",
-                address: "123"
-            }
-        });
+        axiosMock.onPost("/api/attractions/post").reply( 202, attraction );
 
-        render(
+        const { getByTestId } = render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
                     <AttractionCreatePage />
                 </MemoryRouter>
             </QueryClientProvider>
-        )
+        );
 
-        const nameInput = screen.getByLabelText("Name");
-        expect(nameInput).toBeInTheDocument();
+        await waitFor(() => {
+            expect(getByTestId("AttractionForm-address")).toBeInTheDocument();
+        });
 
+        const addressField = getByTestId("AttractionForm-address");
+        const nameField = getByTestId("AttractionForm-name");
+        const descriptionField = getByTestId("AttractionForm-description");
+        const submitButton = getByTestId("AttractionForm-submit");
 
-        const descriptionInput = screen.getByLabelText("Description");
-        expect(descriptionInput).toBeInTheDocument();
+        fireEvent.change(addressField, { target: { value: '1000' } });
+        fireEvent.change(nameField, { target: { value: 'Inkstriker' } });
+        fireEvent.change(descriptionField, { target: { value: 'kart' } });
 
-        const addressInput = screen.getByLabelText("Address");
-        expect(descriptionInput).toBeInTheDocument();
+        expect(submitButton).toBeInTheDocument();
 
-        const createButton = screen.getByText("Create");
-        expect(createButton).toBeInTheDocument();
+        fireEvent.click(submitButton);
 
-        
-        fireEvent.change(nameInput, { target: { value: 'South Coast Deli' } })
-        fireEvent.change(descriptionInput, { target: { value: 'Sandwiches and Salads' } })
-        fireEvent.change(addressInput, { target: { value: '123' } })
-        fireEvent.click(createButton);
-        
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
 
-        await waitFor(() => expect(mockAdd).toHaveBeenCalled());
-        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/attractions"));
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+            "name": "Inkstriker",
+            "description": "kart",
+            "address": "1000"
+        });
 
-        // assert - check that the console.log was called with the expected message
-        expect(console.log).toHaveBeenCalled();
-        const message = console.log.mock.calls[0][0];
-        const expectedMessage =  `createdAttraction: {"attraction":{"id":3,"name":"South Coast Deli","description":"Sandwiches and Salads","address":"123"}}`
-
-        expect(message).toMatch(expectedMessage);
-        restoreConsole();
-
+        expect(mockToast).toBeCalledWith("New attraction Created - id: 17 name: Inkstriker");
+        expect(mockNavigate).toBeCalledWith({ "to": "/attractions/list" });
     });
 
+
 });
+
+
 
 
