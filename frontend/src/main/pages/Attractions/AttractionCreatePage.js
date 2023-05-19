742fc54477e2b394127934fa2a5622a10bb588e6
SophiaWeiler <sophia.weiler@outlook.com>
2023-05-19 12:34:49 -0700

    sw - changes to AttractionCreatePage + test to connect backend

--- frontend/src/main/pages/Attractions/AttractionCreatePage.js
+++ frontend/src/main/pages/Attractions/AttractionCreatePage.js
@@ -1,17 +1,44 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import AttractionForm from "main/components/Attractions/AttractionForm";
-import { useNavigate } from 'react-router-dom'
-import { attractionUtils } from 'main/utils/attractionUtils';
+import { Navigate/*, useNavigate */ } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+//import { useNavigate } from 'react-router-dom'
+//import { attractionUtils } from 'main/utils/attractionUtils';
 
 export default function AttractionCreatePage() {
 
-  let navigate = useNavigate(); 
 
-  const onSubmit = async (attraction) => {
-    const createdAttraction = attractionUtils.add(attraction);
-    console.log("createdAttraction: " + JSON.stringify(createdAttraction));
-    navigate("/attractions");
-  }  
+  const objectToAxiosParams = (attraction) => ({
+    url: "/api/attractions/post",
+    method: "POST",
+    params: {
+      name: attraction.name,
+      description: attraction.description,
+      address: attraction.address
+    }
+  });
+
+  const onSuccess = (attraction) => {
+    toast(`New attractions Created - id: ${attraction.id} name: ${attraction.name}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/attractions/all"]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/attractions/list" />
+  }
 
   return (
     <BasicLayout>
---
+++ frontend/src/tests/pages/Attractions/AttractionCreatePage.test.js
@@ -0,0 +1,104 @@
+import { render, waitFor, fireEvent } from "@testing-library/react";
+import AttractionCreatePage from "main/pages/Attractions/AttractionCreatePage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
+describe("AttractionCreatePage tests", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    beforeEach(() => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    });
+
+    test("renders without crashing", () => {
+        const queryClient = new QueryClient();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AttractionCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+
+        const queryClient = new QueryClient();
+        const transport = {
+            id: 17,
+            name: "Inkstriker",
+            description: "kart",
+            address: "1000"
+        };
+
+        axiosMock.onPost("/api/attractions/post").reply( 202, transport );
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AttractionCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => {
+            expect(getByTestId("AttractionForm-address")).toBeInTheDocument();
+        });
+
+        const addressField = getByTestId("AttractionForm-address");
+        const nameField = getByTestId("AttractionForm-name");
+        const descriptionField = getByTestId("AttractionForm-description");
+        const submitButton = getByTestId("AttractionForm-submit");
+
+        fireEvent.change(addressField, { target: { value: '1000' } });
+        fireEvent.change(nameField, { target: { value: 'Inkstriker' } });
+        fireEvent.change(descriptionField, { target: { value: 'kart' } });
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+            "name": "Inkstriker",
+            "description": "kart",
+            "address": "1000"
+        });
+
+        expect(mockToast).toBeCalledWith("New attractions Created - id: 17 name: Inkstriker");
+        expect(mockNavigate).toBeCalledWith({ "to": "/attractions/list" });
+    });
+
+
+});
