2e7c2792086009dd4c81709df125d68ca8a0e51e
Mikenike5313 <michaelkuhn@Michaels-MacBook-Pro.local>
2023-05-18 18:40:09 -0700

    details page + tests integrated

--- frontend/src/main/pages/Transports/TransportDetailsPage.js
+++ frontend/src/main/pages/Transports/TransportDetailsPage.js
@@ -1,18 +1,53 @@
+// import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+// import { useParams } from "react-router-dom";
+// import TransportTable from 'main/components/Transports/TransportTable';
+// import { transportUtils } from 'main/utils/transportUtils';
+
+// export default function TransportDetailsPage() {
+//   let { id } = useParams();
+
+//   const response = transportUtils.getById(id);
+
+//   return (
+//     <BasicLayout>
+//       <div className="pt-2">
+//         <h1>Transport Details</h1>
+//         <TransportTable transports={[response.transport]} showButtons={false} />
+//       </div>
+//     </BasicLayout>
+//   )
+// }
+
+
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
 import TransportTable from 'main/components/Transports/TransportTable';
-import { transportUtils } from 'main/utils/transportUtils';
+import { useBackend } from "main/utils/useBackend";
 
 export default function TransportDetailsPage() {
   let { id } = useParams();
 
-  const response = transportUtils.getById(id);
+  const { data: transport, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/transport?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/transport`,
+        params: {
+          id
+        }
+      }
+    );
 
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Transport Details</h1>
-        <TransportTable transports={[response.transport]} showButtons={false} />
+        {
+          transport && <TransportTable transports={[transport]} showButtons={false} />
+        }
       </div>
     </BasicLayout>
   )
